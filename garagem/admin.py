8ec03d9ab4d525f038a9225c2a0370e8543440f8
alessandrafr4 <alessandralelehfr495@gmail.com>
2023-03-28 14:59:12 -0300

    criacao do modelo cor

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from garagem.models import Acessorio, Marca, Categoria
+from garagem.models import Cor, Acessorio, Marca, Categoria
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
 admin.site.register(Acessorio)
+admin.site.register(Cor)
 
 
 from django.contrib import admin
--- garagem/models.py
+++ garagem/models.py
@@ -14,7 +14,16 @@ class Categoria(models.Model):
         return self.descricao
     
 class Acessorio(models.Model):
-    descricao = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
+    
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao 
+    
+    class Meta:
+        verbose_name_plural = "Cores"
