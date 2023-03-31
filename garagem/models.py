be7422e25a76f865f7f7b47c6e4edb5ea817bf02
alessandrafr4 <alessandralelehfr495@gmail.com>
2023-03-31 14:11:18 -0300

    criacao modelo veiculo

--- garagem/models.py
+++ garagem/models.py
@@ -29,19 +29,16 @@ class Cor(models.Model):
         verbose_name_plural = "Cores"
 
 class Veiculo(models.Model):
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="livros")
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="livros")
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos" )
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor,on_delete=models.PROTECT, related_name="veiculos")
+    ano = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+    modelo = models.CharField(max_length=50)
 
-    ano = models.IntegerField(max_length=32, null=True, blank=True)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-
-    
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
-    Editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
 
 
     def __str__(self):
-        return f"{self.nome.upper()} ({self.id})"
+        return f"Modelo:{self.modelo} Cor:{self.cor} Ano:{self.ano} Marca:{self.marca}" 
 
         
