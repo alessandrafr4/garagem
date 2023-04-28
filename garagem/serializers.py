e7ab46116b220d8095f0f5c2493e0b2b5be3fcfd
alessandrafr4 <alessandralelehfr495@gmail.com>
2023-04-28 14:14:13 -0300

    Criacao da API cores

--- garagem/serializers.py
+++ garagem/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 
-from garagem.models import Categoria, Marca 
+from garagem.models import Categoria,Cor, Marca 
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria 
         fields = "__all__"
 
+class CorSerializer(ModelSerializer):
+    class Meta:
+        model = Cor 
+        fields = "__all__"
+ 
+
 class MarcaSerializer(ModelSerializer):
     class Meta:
         model = Marca 
         fields = "__all__"
- 
