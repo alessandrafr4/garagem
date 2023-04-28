48fdadb447fddf82bd470b7f746f1a8dd2842c5a
alessandrafr4 <alessandralelehfr495@gmail.com>
2023-04-28 14:09:11 -0300

    Criacao da API Categoria

--- garagem/views.py
+++ garagem/views.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Marca
-from garagem.serializers import MarcaSerializer
+from garagem.models import Categoria,Marca
+from garagem.serializers import CategoriaSerializer,MarcaSerializer
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer 
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
-    serializer_class = MarcaSerializer
+    serializer_class = MarcaSerializer 
