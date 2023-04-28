c96ff13ebf4f34f451e3341ca2c71ae45effb9e6
alessandrafr4 <alessandralelehfr495@gmail.com>
2023-04-28 14:22:18 -0300

    Ciacao da API Acessorio

--- garagem/views.py
+++ garagem/views.py
@@ -1,7 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Categoria,Cor,Marca
-from garagem.serializers import CategoriaSerializer,CorSerializer, MarcaSerializer
+from garagem.models import Acessorio,Categoria,Cor,Marca
+from garagem.serializers import  AcessorioSerializer,CategoriaSerializer,CorSerializer, MarcaSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer 
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
