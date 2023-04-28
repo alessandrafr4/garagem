62a7596d9f1b92a63f505532b7bcfc66db6205b3
alessandrafr4 <alessandralelehfr495@gmail.com>
2023-04-28 14:31:23 -0300

    Criacao da API Veiculos

--- config/urls.py
+++ config/urls.py
@@ -3,13 +3,14 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import AcessorioViewSet,CategoriaViewSet,CorViewSet,MarcaViewSet
+from garagem.views import AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet, VeiculoViewSet
 
 router = DefaultRouter()
 router.register(r"acessorios", AcessorioViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"cores", CorViewSet)
 router.register(r"marcas", MarcaViewSet)
+router.register(r"veiculos", VeiculoViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- garagem/views.py
+++ garagem/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Acessorio,Categoria,Cor,Marca
-from garagem.serializers import  AcessorioSerializer,CategoriaSerializer,CorSerializer, MarcaSerializer
+from garagem.models import Acessorio,Categoria, Cor, Marca, Veiculo
+from garagem.serializers import  AcessorioSerializer, CategoriaSerializer, CorSerializer, MarcaSerializer, VeiculoSerializer
 
 class AcessorioViewSet(ModelViewSet):
     queryset = Acessorio.objects.all()
@@ -19,3 +19,9 @@ class CorViewSet(ModelViewSet):
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer 
+
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer 
+
